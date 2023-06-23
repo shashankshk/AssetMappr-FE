e1e6420bc07a62e469fe477aec41498768e72570
sharoni <sharoni.royc@gmail.com>
2023-06-22 21:29:33 -0400

    Input component: for input field, type and label

---
+++ src/components/input/InputField.tsx
@@ -0,0 +1,37 @@
+import React, { RefObject } from 'react';
+
+interface Props {
+    type: string;
+    id?: string;
+    className?:string;
+    placeholder: string;
+    ref?: RefObject<HTMLInputElement>;
+    autocomplete?: string;
+    onchange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    required?: boolean
+}
+
+const InputField: React.FC<Props> = ({
+    type,
+    id,
+    className,
+    placeholder,
+    ref,
+    autocomplete,
+    onchange,
+    required
+}) => {
+    return (
+        <input type={type} 
+        id={id} 
+        className={className} 
+        placeholder={placeholder}
+        ref = {ref} 
+        autoComplete={autocomplete}
+        onChange={onchange}
+        required={required}
+        ></input>
+    )
+};
+
+export default InputField;
