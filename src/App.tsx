c70db6d9354d84759b6c5b635eee319678d2300c
sharoni <sharoni.royc@gmail.com>
2023-06-22 21:39:08 -0400

    Modified routing names for screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Welcome from './components/welcome/Welcome';
-import WelcomeScreen from './components/welcome2/Welcome2';
-import Signup from './components/signup/Signup';
-import Signupcontinue from './components/signup/Signupcont';
-import WelcomeContScreen from './components/welcome3/Welcome3';
-import DisclaimerScreen from './components/disclaimer/Disclaimer';
-import ProfileInfoScreen from './components/profile/ProfileInfo';
-import Login from './components/login/Login';
-import HomeScreen from './components/home/Home';
+import Welcome from './screens/welcome/Welcome';
+import WelcomeScreen from './screens/welcomecont/WelcomeCont';
+import Signup from './screens/signup/Signup';
+import Signupcontinue from './screens/signup/Signupcont';
+import WelcomeOnSignupScreen from './screens/welcomeonsignup/WelcomeOnSignup';
+import DisclaimerScreen from './screens/disclaimer/Disclaimer';
+import ProfileInfoScreen from './screens/profile/ProfileInfo';
+import Login from './screens/login/Login';
+import HomeScreen from './screens/home/Home';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Welcome/>} />
-      <Route path="welcome2" element={<WelcomeScreen/>} />
+      <Route path="welcomecont" element={<WelcomeScreen/>} />
       <Route path="signup" element={<Signup/>} />
       <Route path="login" element={<Login/>} />
       <Route path="signupcontinue" element={<Signupcontinue/>} />
-      <Route path="welcome3" element={<WelcomeContScreen/>} />
+      <Route path="welcome3" element={<WelcomeOnSignupScreen/>} />
       <Route path="disclaimer" element={<DisclaimerScreen/>} />
       <Route path="profile" element={<ProfileInfoScreen/>} />
       <Route path="home" element={<HomeScreen/>} />
